9a179eb98243cff4cc876725dd19b617e80117ee
insanehead <insanehead@gmail.com>
2014-07-28 23:01:27 +0530

    covered till vid number 10

--- AnotherStart/src/com/example/anotherstart/MainActivity.java
+++ AnotherStart/src/com/example/anotherstart/MainActivity.java
@@ -4,14 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+	int counter;
+	Button add, sub;
+	TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
+        add = (Button) findViewById(R.id.button1);
+        sub = (Button) findViewById(R.id.button2);
+        display = (TextView) findViewById(R.id.textView1);
+        add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter++;
+				display.setText("Your total is now " + counter);
+			}
+		});
+        
+        sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				counter--;
+				display.setText("Your total is now " + counter);
+			}
+		});
     }
 
 
